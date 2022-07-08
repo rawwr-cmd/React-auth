eff2c49ae0da119fc2ab10b2d0fa2e1922f0f359
rawwr <akshayace0302@gmail.com>
2022-07-08 18:25:15 +0530

    persisting the user authentication status

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -7,17 +7,20 @@ const AuthContext = React.createContext({
 });
 
 export const AuthContextProvider = (props) => {
-  const [token, setToken] = useState(null);
+  const initialToken = localStorage.getItem("token");
+  const [token, setToken] = useState(initialToken);
 
   //!! converts truthy and falsy into a truthy/falsy boolean
   const userIsLoggedIn = !!token;
 
   const loginHandler = (token) => {
     setToken(token);
+    localStorage.setItem("token", token);
   };
 
   const logoutHandler = () => {
     setToken(null);
+    localStorage.removeItem("token");
   };
 
   const contextValue = {
